4d55ae65239127f348b18c72df8b3c73f139ee75
yashi8484 <kobayashi@giftee.co>
2021-09-26 22:27:09 +0900

    Complete "Using Generics" chapter
    https://www.fastify.io/docs/latest/TypeScript/#using-generics

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,39 @@
 import fastify from "fastify";
 
+interface IQuerystring {
+  username: string;
+  password: string;
+}
+
+interface IHeaders {
+  "h-Custom": string;
+}
+
 const server = fastify();
 
 server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
+
+server.get<{
+  Querystring: IQuerystring;
+  Headers: IHeaders;
+}>(
+  "/auth",
+  {
+    preValidation: (request, reply, done) => {
+      const { username, password } = request.query;
+      done(username !== "admin" ? new Error("Must be admin") : undefined); // only validate 'admin' account
+    },
+  },
+  async (request, reply) => {
+    const customerHeader = request.headers["h-Custom"];
+
+    // do something with request data
+
+    return `logged in!\n`;
+  }
+);
 
 server.listen(8080, (err, address) => {
   if (err) {
