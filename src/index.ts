fb513e6845d54a22ebca5bfdf4e6946d147944c0
yashi8484 <kobayashi@giftee.co>
2021-10-20 19:09:34 +0900

    Remove unnecessary packages

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,10 @@
 import fastify from "fastify";
 
-import QuerystringSchema from "../schemas/querystring.json";
-import HeadersSchema from "../schemas/headers.json";
-
-import { QuerystringSchema as QuerystringSchemaInterface } from "../types/querystring";
-import { HeadersSchema as HeadersSchemaInterface } from "../types/headers";
-
 const server = fastify();
 
 server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
-
-server.get<{
-  Querystring: QuerystringSchemaInterface;
-  Headers: HeadersSchemaInterface;
-}>(
-  "/auth",
-  {
-    schema: {
-      querystring: QuerystringSchema,
-      headers: HeadersSchema,
-    },
-    preValidation: (request, reply, done) => {
-      const { username, password } = request.query;
-      done(username !== "admin" ? new Error("Must be admin") : undefined); // only validate 'admin' account
-    },
-  },
-  async (request, reply) => {
-    const customerHeader = request.headers["h-Custom"];
-
-    // do something with request data
-
-    return `logged in!\n`;
-  }
-);
 
 if (process.env.NODE_ENV === "production") {
   server.listen(8080, (err, address) => {
